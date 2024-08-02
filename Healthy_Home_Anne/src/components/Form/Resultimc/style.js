01f4430dd499909d580a05598fd5f3e548717efa
vlan-souza <vlan_souza@outlook.com>
2024-08-02 11:03:20 -0300

    Realização do projeto de extensão de aplicativo

---
+++ Healthy_Home_Anne/src/components/Form/Resultimc/style.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+const styles= StyleSheet.create({
+	contextImc: {
+		flex:1,
+		marginTop:20,
+		paddingTop:15,
+		alignItems:"center",
+		width:"100%",
+	},
+	ResultImc:{
+		fontSize:48,
+		color:"#ff0043",
+		fontWeight:"bold",
+	},
+	numberImc: {
+		fontSize:48,
+		color:"#ff0043",
+		fontWeight:"bold",
+	},
+	information: {
+		fontSize:18,
+		color:"#ff0043",
+		fontWeight:"bold",
+	},
+	boxSharebutton:{
+		width:"100%",
+		alignItems:"center",
+		marginBotton:10,
+	},
+	shared:{
+		backgroundColor:"#1877f2",
+	 	borderRadius: 50,
+		paddingBottom: 5,
+		paddingTop: 5,
+	},
+	sharedText:{
+		Color:"#ffffff",
+		fontWeight:"bold",
+	 	paddingHorizontal: 50,
+		
+	}
+});
+
+export default styles
---
+++ Healthy_Home_Anne/src/components/Title/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	boxTitle: {
+		alignContent: "center",
+		justifyContent:"center",
+		padding:10,
+	},
+	textTitle:{
+		color:"#FF0043",
+		fontSize:24,
+		fontWeight:"bold",
+		
+	}
+
+
+
+
+});
+export default styles
